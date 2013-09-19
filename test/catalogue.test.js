cd134237ab0a389ad112152d25daa02769edbb04
Yassine Azzout <92bondstreet@gmail.com>
2013-09-19 14:19:58 +0200

    update calling methods

--- test/catalogue.test.js
+++ test/catalogue.test.js
@@ -1,5 +1,5 @@
 var should = require('should');
-var PubMonVoisin = require('../lib/main');
+var postman = require('../lib/main');
 
 
 
@@ -16,11 +16,8 @@ var traceError = function (error, retval) {
  *    DEFINITION OF OBJECT PARAMETER
  */
 
-var who = { "full_title" : "MR",
-            "title" : "2",
-            "firstname" : "Joseph",
-            "lastname" : "SOUCY",
-            "complete_address" : "64 boulevard Aristide Briand",
+var who = { "firstname" : "Joseph",
+            "lastname" : "SOUCY",            
             "number_address" : "64",
             "address" : "boulevard Aristide Briand",
             "zipcode" : "76120",
@@ -33,7 +30,6 @@ describe('letterBox', function() {
     describe('with no arguments', function() {
         it('throw ERROR', function() {
           (function () {
-            var postman = new PubMonVoisin();
             postman.letterBox();
           }).should.throw();  
         });
@@ -41,15 +37,13 @@ describe('letterBox', function() {
 
     describe('with number argument different to 2 or 3', function() {
         it('throw ERROR', function() {
-          (function () {
-            var postman = new PubMonVoisin();
+          (function () {          
             postman.letterBox("");
           }).should.throw();
         });
 
          it('throw ERROR', function() {
           (function () {
-            var postman = new PubMonVoisin();
             postman.letterBox("","", "", "");
           }).should.throw();
         });
@@ -58,7 +52,6 @@ describe('letterBox', function() {
     describe('With 3 invalids arguments', function() {
         it('throw ERROR', function() {
           (function () {
-            var postman = new PubMonVoisin();
             postman.letterBox("","","");
           }).should.throw();
         });
@@ -67,22 +60,19 @@ describe('letterBox', function() {
     describe('With 3 correct arguments', function() {
         it('callback if JSON Object for recipient is not correct', function() {   
           (function () {
-            var postman = new PubMonVoisin();
             postman.letterBox("",what, traceError);
           }).should.throw();     
         });
         it('return OK if args are correct', function() {        
-          var postman = new PubMonVoisin();
           postman.letterBox(who, what,traceError).should.be.true;
         });
         it('build complete WHO information', function() {        
-          var postman = new PubMonVoisin();
           var recipient = postman.buildRecipient(who);
-          recipient.should.have.property('email');
+          recipient.should.have.property('complete_address');
+          recipient.should.have.property('email');          
           recipient.should.have.property('birthday');
         });
         it('callback if recipient is register to catalogs provided', function(done) {        
-          var postman = new PubMonVoisin();
           postman.letterBox(who,function(err){
             if (err) console.log(err);
             done();
@@ -90,7 +80,6 @@ describe('letterBox', function() {
         });
         it('callback if recipient is register', function(done) {        
           this.timeout(50000);
-          var postman = new PubMonVoisin();
           postman.letterBox(who,['ullapopken'/*'madeleine','damart','camif','bonprixservice','afibel','bleu-bonheur'*/],function(err){
             if (err) console.log(err);
             done();
